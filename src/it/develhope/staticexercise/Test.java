67dea1ee810705dde8df138ed36497a6ab51a9c7
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-15 16:20:24 +0100

    static

--- src/it/develhope/staticexercise/Test.java
+++ src/it/develhope/staticexercise/Test.java
@@ -6,11 +6,13 @@ public class Test {
 
         Employee employee1 = new Employee("Lorenzo", "Balestrini", "Piazza Paolo da Novi");
         Employee employee2 = new Employee("Silvia", "Bagni", "Piazza Palermo");
-        Badge badge1 = new Badge(employee1);
-        Badge badge2 = new Badge(employee2);
 
+        Badge badge1 = new Badge(employee1);
         badge1.showBadgeDetails();
+
         System.out.println("---------------");
+
+        Badge badge2 = new Badge(employee2);
         badge2.showBadgeDetails();
     }
 }
